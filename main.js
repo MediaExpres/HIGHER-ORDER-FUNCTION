30050d201794a40316cd867ed50883e635fc8446
Mihai Pavelescu <media.expres@gmail.com>
2023-08-01 21:46:25 +0300

    refactoring

--- main.js
+++ main.js
@@ -18,12 +18,27 @@
 // letAdamLogin()
 // letEvaLogin()
 
+// ........................
+// function letUserLogin(user) {
+//     let array = [];
+//     for (let i = 0; i < 10000000; i++) {
+//         array.push(i)
+//     }
+//     return 'Aces Granted to ' + user;
+// }
+
+// letUserLogin('Adam')
+
+// .......... refactoring
+const giveAccessTo = (name) => { // wre now tell functioni what data to use
+'Access Granted to ' + name;
+}
 function letUserLogin(user) {
     let array = [];
     for (let i = 0; i < 10000000; i++) {
         array.push(i)
     }
-    return 'Aces Granted to ' + user;
+    return giveAccessTo(user);
 }
 
 letUserLogin('Adam')
