f0e25448b3ba2cb5e34b707ae2293222b5c2a5d8
Mihai Pavelescu <media.expres@gmail.com>
2023-08-01 21:40:24 +0300

    more reasonable second grade

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-function letAdamLogin() {
+// function letAdamLogin() {
+//     let array = [];
+//     for (let i = 0; i < 10000000; i++) {
+//         array.push(i)
+//     }
+//     return 'Aces Granted to Adam'
+// }
+
+// // function letEvaLogin() {
+// //     let array = [];
+// //     for (let i = 0; i < 10000000; i++) {
+// //         array.push(i)
+// //     }
+// //     return 'Aces Granted to Eva'
+// // }
+
+
+// letAdamLogin()
+// letEvaLogin()
+
+function letUserLogin(user) {
     let array = [];
     for (let i = 0; i < 10000000; i++) {
         array.push(i)
     }
-    return 'Aces Granted to Adam'
+    return 'Aces Granted to ' + user;
 }
 
-letAdamLogin()
+letUserLogin('Adam')
