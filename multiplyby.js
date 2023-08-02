f4a8482988b34068a74a55b18cbe67caa8c6fcd3
Mihai Pavelescu <media.expres@gmail.com>
2023-08-02 10:50:12 +0300

    removed curly braces

--- multiplyby.js
+++ multiplyby.js
@@ -1,13 +1,20 @@
 // multiplyBy function in the higher order function lesson
 
-const multiplyBy = function(num1) {
-    return function(num2) {
-        return num1*num2
-    }
-}
+// const multiplyBy = function(num1) {
+//     return function(num2) {
+//         return num1*num2
+//     }
+// }
+
+// const multiplyByTwo = multiplyBy(2);
+
+// multiplyByTwo(2);
+// multiplyByTwo(4);
+// multiplyByTwo(10);
+
+// the same with arrow functions
+
+const multiplyBy = (num1) => (num2) => num1*num2
 
 const multiplyByTwo = multiplyBy(2);
-
-multiplyByTwo(2);
-multiplyByTwo(4);
-multiplyByTwo(10);
+multiplyByTwo(6);
