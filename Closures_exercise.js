22a17715ec968a5d82c84400896af5c5707f8693
Mihai Pavelescu <media.expres@gmail.com>
2023-08-03 09:22:01 +0300

    moving const definition around

--- Closures_exercise.js
+++ Closures_exercise.js
@@ -1,8 +1,11 @@
 function callMeMaybe() {
-    const callMe = 'Hi, I am now, here!';
+  //  const callMe = 'Hi, I am now, here!';
     setTimeout(function() {
         console.log(callMe);
         }, 4000); // 4000 stands for 4 seconds
-}
+
+        const callMe = 'Hi, I am now, here!'; // closure works even now in this positiom !!!!
+
+    }
 
 callMeMaybe(); //wait 4 seconds and then print Hi, I am now, here!
