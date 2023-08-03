27053e38afd9ab72dd90c6086eb049752807ab62
Mihai Pavelescu <media.expres@gmail.com>
2023-08-03 08:56:18 +0300

    closure explanation

--- closures.js
+++ closures.js
@@ -2,6 +2,7 @@ function a() {
     let grandpa = 'grandpa'
     return function b() {
 let father = 'father'
+let random = 3212315 // will be garbaged because nothing is referencing it in the child below. grandpa and father not because they are referenced below
 return function c() {
     let son = 'son'
     return `${grandpa} > ${father} > ${son}`
