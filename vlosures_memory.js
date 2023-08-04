4a6734b1272fe41e1055fde258753c6c69c65ecc
Mihai Pavelescu <media.expres@gmail.com>
2023-08-04 19:02:25 +0300

    added closure

--- vlosures_memory.js
+++ vlosures_memory.js
@@ -8,7 +8,17 @@ function heavyDuty(index) {
 heavyDuty(688)
 heavyDuty(688)
 heavyDuty(688)
-heavyDuty(688)
+const getHeavyDuty = heavyDuty2() // create a variable that gets heavyDuty2 and () call this
+getHeavyDuty(688)
+getHeavyDuty(700)
+getHeavyDuty(800)
 
+function heavyDuty2() {
+    const bigArray = new Array(7000).fill(':)')
+    console.log('created Again')
+    return function(index) {
+        return bigArray[index]
+    }
+} 
 
 // allows ENCAPSULATION
