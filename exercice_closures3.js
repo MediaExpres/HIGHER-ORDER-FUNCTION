725e8a82ee1f9c777c3d2a9b5ac86767d58e037f
Mihai Pavelescu <media.expres@gmail.com>
2023-08-07 16:35:03 +0300

    second solution with closures

--- exercice_closures3.js
+++ exercice_closures3.js
@@ -12,10 +12,22 @@
 // I'm at the index 1
 // I'm at the index 2
 // I'm at the index 3
+// because let allows to block scoping between curly brackets below create a scope for each i who then is scoped below
 
-const array = [1, 2, 3, 4];
-for(let i=0; i<array.lenght; i++) {
+// const array = [1, 2, 3, 4];
+// for(let i=0; i<array.lenght; i++) {
+//     setTimeout(function() {
+//         console.log('I\'m at the index ' + i) // to log the values write array[i] instead of i
+//     }, 3000)
+// } 
+
+// second solution with var and closure
+
+const array = [1,2,3,4];
+for(var i=0; i < array.length; i++) {
+    (function(closureI) {
     setTimeout(function() {
-        console.log('I\'m at the index ' + i) // to log the values write array[i] instead of i
+        console.log('I\'m at the value ' + array[closureI]) 
     }, 3000)
+})(i)
 } 
