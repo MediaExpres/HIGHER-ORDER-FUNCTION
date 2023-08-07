32a854fee107f891874babe15f1f1ff907ce2eae
Mihai Pavelescu <media.expres@gmail.com>
2023-08-07 16:20:10 +0300

    request added

--- exercice_closures3.js
+++ exercice_closures3.js
@@ -1,4 +1,4 @@
-//
+// the following throws 4 times 4VM75:4 I'm at the index 4. How to show each number?
 
 const array = [1, 2, 3, 4];
 for(var i=0; i<array.lenght; i++) {
@@ -6,3 +6,4 @@ for(var i=0; i<array.lenght; i++) {
         console.log('I\'m at the index ' + i)
     }, 3000)
 } 
+
