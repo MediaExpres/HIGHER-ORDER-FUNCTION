8fcc0147313783071e89049d48ba4a3bb8d6e661
Mihai Pavelescu <media.expres@gmail.com>
2023-08-08 14:01:19 +0300

    going baack up to the object

--- prototypes_inheritance.js
+++ prototypes_inheritance.js
@@ -48,3 +48,24 @@ with:ƒ with()
 Symbol(Symbol.iterator):ƒ values()
 Symbol(Symbol.unscopables):{at: true, copyWithin: true, entries: true, fill: true, find: true, …}
 [[Prototype]]:Object
+
+// if u go back from the array to the object:
+const array = []
+array.__proto__.__proto__
+
+// you get:
+{constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
+constructor: ƒ Object()
+hasOwnProperty: ƒ hasOwnProperty()
+isPrototypeOf: ƒ isPrototypeOf()
+propertyIsEnumerable: ƒ propertyIsEnumerable()
+toLocaleString: ƒ toLocaleString()
+toString: ƒ toString()
+valueOf: ƒ valueOf()
+__defineGetter__: ƒ __defineGetter__()
+__defineSetter__: ƒ __defineSetter__()
+__lookupGetter__: ƒ __lookupGetter__()
+__lookupSetter__: ƒ __lookupSetter__()
+__proto__: (...)
+get __proto__: ƒ __proto__()
+set __proto__: ƒ __proto__()
