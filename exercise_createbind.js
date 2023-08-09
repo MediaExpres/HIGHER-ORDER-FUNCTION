75675bbe4f9a046d068a379c8b8b575794de9e79
Mihai Pavelescu <media.expres@gmail.com>
2023-08-09 19:29:24 +0300

    resolution with Aria and Neagoie

--- exercise_createbind.js
+++ exercise_createbind.js
@@ -4,3 +4,20 @@
 
 // Function.prototype.bind = function(){
 // }
+
+// result by Aria in Opera:
+Function.prototype.bind = function(context, ...args) {
+    const originalFunction = this; // reference to the original function
+  
+    return function(...innerArgs) {
+      return originalFunction.apply(context, [...args, ...innerArgs]);
+    };
+  };
+
+  // result by Andrei:
+  Function.prototype.bind = function(whoIsCallingMe){
+    const self = this;
+    return function(){
+      return self.apply(whoIsCallingMe, arguments);
+    };
+  }
