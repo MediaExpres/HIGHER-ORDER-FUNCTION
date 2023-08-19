e3ad9a7fe91b7d6a834736d19cdb427b63a1f5a6
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:44:36 +0300

    Capital Elf to mark constructor and explain errors

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -1,13 +1,15 @@
    // Constructor Funcrtions
    
-function elf(name, weapon) {
+function Elf(name, weapon) {
     this.name = name;
     this.weapon = weapon;  
 }
     
-const peter = new elf('Peter', 'stones') //SyntaxError: Identifier 'elf' has already been declared
+const peter = new Elf('Peter', 'stones') //SyntaxError: Identifier 'elf' has already been declared
   
-const sam = new elf('Samuel', 'fire')
+const sam = new Elf('Samuel', 'fire')
 sam.name
     // WHY? SyntaxError: Identifier 'elf' has already been declared at <anonymous>:1:1
     // BUT IN FIREFOX WORKS, now Chrome is more strict with old methods?
+    // BUT then in Firefox throws SyntaxError: redeclaration of const peter ?!? why?
+    // ANSWER: forgot to reload the browser :D 
