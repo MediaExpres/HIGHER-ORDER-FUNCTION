c2d125a41aa125c7ceda2330d53b6588c0ea5af0
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:18:15 +0300

    error in contrast with the no error in lecture

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -1,15 +1,12 @@
    // Constructor Funcrtions
    
     function elf(name, weapon) {
-      this.name = name;
-      this.weapon = weapon;
-    }
+        this.name = name;
+        this.weapon = weapon;  
+}
     
-    const Peter = new elf('Peter', 'stones');
-    // Peter.attack = elfFunctions.attack
-    // console.log(Peter.attack())
+    const Peter = new elf('Peter', 'stones'); //SyntaxError: Identifier 'elf' has already been declared
+  
+    const Sam = new elf('Samuel', 'fire');
+ 
     
-    const Sam = new elf('Samuel', 'fire')
-    // Sam.attack = elfFunctions.attack // this one is added after introduction of elfFunctions
-    // console.log(Sam.attack())
-    Sam.name // if run this error cannot read property name so you need to add new above to the elfes
