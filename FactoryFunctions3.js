4f1feb557ec2bd82a4ecdf49629c2cce5bb5fdf4
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:28:49 +0300

    But in firefox it works

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -1,12 +1,13 @@
    // Constructor Funcrtions
    
-    function elf(name, weapon) {
-        this.name = name;
-        this.weapon = weapon;  
+function elf(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;  
 }
     
-    const peter = new elf('Peter', 'stones') //SyntaxError: Identifier 'elf' has already been declared
+const peter = new elf('Peter', 'stones') //SyntaxError: Identifier 'elf' has already been declared
   
-    const sam = new elf('Samuel', 'fire')
- sam.name
+const sam = new elf('Samuel', 'fire')
+sam.name
     // WHY? SyntaxError: Identifier 'elf' has already been declared at <anonymous>:1:1
+    // BUT IN FIREFOX WORKS, now Chrome is more strict with old methods?
