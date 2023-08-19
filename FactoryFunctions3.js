10051ccacf369505477da06402e53d4452b1fa70
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:59:02 +0300

    adding attack to Elf

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -4,11 +4,16 @@ function Elf(name, weapon) {
     this.name = name;
     this.weapon = weapon;  
 }
-    
+
+Elf.prototype.attack = function() {
+    return 'attack with ' + this.weapon
+}
+
 const peter = new Elf('Peter', 'stones') //SyntaxError: Identifier 'elf' has already been declared
   
 const sam = new Elf('Samuel', 'fire')
-sam.name
+//sam.name
+sam.attack
     // WHY? SyntaxError: Identifier 'elf' has already been declared at <anonymous>:1:1
     // BUT IN FIREFOX WORKS, now Chrome is more strict with old methods?
     // BUT then in Firefox throws SyntaxError: redeclaration of const peter ?!? why?
@@ -17,12 +22,12 @@ sam.name
 
 // Example natively construction in javascript is Function()
 
-const Elf1 = new Function('name', 'weapon', 
-`this.name = name;
-this.weapon = weapon;`)
+// const Elf1 = new Function('name', 'weapon', 
+// `this.name = name;
+// this.weapon = weapon;`)
 
-const sara = new Elf1('Sarach', 'fireworks')
-sara
+// const sara = new Elf1('Sarach', 'fireworks')
+// sara
 
 // throws {name: 'Sarach', weapon: 'fireworks'}
 // name : "Sarach"
