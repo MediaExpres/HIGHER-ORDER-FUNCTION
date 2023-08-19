4540639e4dc0d20ef60dc12b4543752024de2411
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:05:57 +0300

    error Sam.name

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -19,23 +19,17 @@
     elf2.attack()
     
     // Constructor Funcrtions
-    const elfFunctions = {
-        attack() {
-            return 'attack with ' + this.weapon // this. added after error: weapon is not defined
-        }
-    }
-    function createElf(name, weapon) {
-        let newElf = Object.create(elfFunctions)
-       newElf.name = name; //: name, symplified with ES6
-       newElf.weapon = weapon;
-       return newElf; //: weapon,
-          // removed the function attack from there and created elfFunctions above
+   
+    function elf(name, weapon) {
+      this.name = name;
+      this.weapon = weapon;
     }
     
-    const Peter = createElf('Peter', 'stones');
+    const Peter = elf('Peter', 'stones');
     // Peter.attack = elfFunctions.attack
-    console.log(Peter.attack())
+    // console.log(Peter.attack())
     
-    const Sam = createElf('Samuel', 'fire')
+    const Sam = elf('Samuel', 'fire')
     // Sam.attack = elfFunctions.attack // this one is added after introduction of elfFunctions
-    console.log(Sam.attack())
+    // console.log(Sam.attack())
+    Sam.name // if run this error cannot read property name so you need to add new above to the elfes
