6b06b30be18997b8778d55952a449d73a56b9eaa
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:52:13 +0300

    example with Function()

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -13,3 +13,18 @@ sam.name
     // BUT IN FIREFOX WORKS, now Chrome is more strict with old methods?
     // BUT then in Firefox throws SyntaxError: redeclaration of const peter ?!? why?
     // ANSWER: forgot to reload the browser :D 
+
+
+// Example natively construction in javascript is Function()
+
+const Elf1 = new Function('name', 'weapon', 
+`this.name = name;
+this.weapon = weapon;`)
+
+const sara = new Elf1('Sarach', 'fireworks')
+sara
+
+// throws {name: 'Sarach', weapon: 'fireworks'}
+// name : "Sarach"
+// weapon : "fireworks"
+// [[Prototype]] : Object
