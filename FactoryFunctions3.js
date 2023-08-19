dc1447feeecc99f5408c5c7d1e602c3919531e0c
Mihai Pavelescu <media.expres@gmail.com>
2023-08-19 13:12:36 +0300

    cleaning up

--- FactoryFunctions3.js
+++ FactoryFunctions3.js
@@ -1,35 +1,15 @@
-// const elf = {
-    name: 'Orwell',
-    weapon: 'Bow',
-    attack() {
-        return 'attack with ' + elf.weapon
-        }
-    }
-    
-    const elf2 = {
-        name: 'Sally',
-        weapon: 'Bow',
-        attack() {
-            return 'attack with ' + elf2.weapon //error: here Andrei kept elf.weapon which points to the first elf not elf2
-            }
-        }
-    
-    // elf.name
-    // elf.attack()
-    elf2.attack()
-    
-    // Constructor Funcrtions
+   // Constructor Funcrtions
    
     function elf(name, weapon) {
       this.name = name;
       this.weapon = weapon;
     }
     
-    const Peter = elf('Peter', 'stones');
+    const Peter = new elf('Peter', 'stones');
     // Peter.attack = elfFunctions.attack
     // console.log(Peter.attack())
     
-    const Sam = elf('Samuel', 'fire')
+    const Sam = new elf('Samuel', 'fire')
     // Sam.attack = elfFunctions.attack // this one is added after introduction of elfFunctions
     // console.log(Sam.attack())
     Sam.name // if run this error cannot read property name so you need to add new above to the elfes
