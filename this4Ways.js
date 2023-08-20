6c4ac4363c2c1b184516a8fcb8b5cc221006c159
Mihai Pavelescu <media.expres@gmail.com>
2023-08-20 13:58:54 +0300

    new binding

---
+++ this4Ways.js
@@ -0,0 +1,10 @@
+// this keyword review
+
+// new binding
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const person1 = new Person('Xanax', 55)
+person1 // Person {name: 'Xanax', age: 55}
