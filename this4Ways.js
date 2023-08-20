6cc8727e61b2dffecbed566e8fa5e2b820a724ef
Mihai Pavelescu <media.expres@gmail.com>
2023-08-20 14:04:09 +0300

    implicite binding

--- this4Ways.js
+++ this4Ways.js
@@ -8,3 +8,14 @@ function Person(name, age) {
 
 const person1 = new Person('Xanax', 55)
 person1 // Person {name: 'Xanax', age: 55}
+
+// implicite binding
+
+const person = {
+    name: 'Karen',
+    age: 40,
+    hi() {
+        console.log('hi ' + this.name)
+    }
+}
+
