ac0543aeb29ef51393661f5cb4e444e2d5fa55ff
Mihai Pavelescu <media.expres@gmail.com>
2023-08-26 14:46:50 +0300

    adding type and super

--- inheritance.js
+++ inheritance.js
@@ -15,6 +15,24 @@
 
 // below is the actual inheritance how you do it
 
+// class Character { // all the characters will have name, weapon and can attack
+//     constructor(name, weapon) {
+//         this.name = name;
+//         this.weapon = weapon;
+//     }
+//     attack() {
+//         return 'attack with ' + this.weapon;
+//     }
+// }
+
+// class Elf extends Character { // subclassing in OOP
+    
+// }
+
+// const fiona = new Elf('Fiona', 'ninja stars');
+// fiona // throws Object { name: "Fiona", weapon: "ninja stars" }
+
+// add type as super class
 class Character { // all the characters will have name, weapon and can attack
     constructor(name, weapon) {
         this.name = name;
@@ -26,8 +44,11 @@ class Character { // all the characters will have name, weapon and can attack
 }
 
 class Elf extends Character { // subclassing in OOP
-    
+    constructor(name, weapon, type) {
+        super(name, weapon);
+        this.type = type
+    }
 }
 
-const fiona = new Elf('Fiona', 'ninja stars');
-fiona // throws Object { name: "Fiona", weapon: "ninja stars" }
+const dolby = new Elf('Dolby', 'cloth', 'house');
+dolby // throws: Object { name: "Dolby", weapon: "cloth", type: "house" }
