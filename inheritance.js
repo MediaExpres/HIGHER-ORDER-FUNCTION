c3ac6a0d753956d4732a559c70b264785e0d360d
Mihai Pavelescu <media.expres@gmail.com>
2023-08-26 14:15:26 +0300

    Inheritance first commit

---
+++ inheritance.js
@@ -0,0 +1,11 @@
+class Elf {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+    attack() {
+        return 'attack with ' + this.weapon;
+    }
+}
+
+const fiona = new Elf('Fiona', 'ninja stars');
