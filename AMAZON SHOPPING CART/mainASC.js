01cc19f7f036f8be3b31d4f8cfa1cfd3c0a52167
Mihai Pavelescu <media.expres@gmail.com>
2023-08-31 10:51:18 +0300

    requirements

---
+++ AMAZON SHOPPING CART/mainASC.js
@@ -0,0 +1,15 @@
+// Amazon shopping
+const user = {
+    name: 'Kim',
+    active: true,
+    cart: [],
+    purchases: [],
+}
+
+purchaseItem 
+
+// Implement cart feature:
+// 1. Add items to cart
+// 2. Add 3% tax on items in cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
