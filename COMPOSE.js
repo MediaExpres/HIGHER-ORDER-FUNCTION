b5dc438d6f6254159645fad6124ec07054eaafbe
Mihai Pavelescu <media.expres@gmail.com>
2023-09-03 09:34:34 +0300

    exact match as lecture throws an error

--- COMPOSE.js
+++ COMPOSE.js
@@ -3,9 +3,9 @@
 // data --> fn --> data --> fn and so on
 // Request: have a number, multyply by 3, absolute which is the same to positive
 
-const compose = (f, g) => (Data) => f(g(Data))
+const compose = (f, g) => (data) => f(g(data))
 const multiplyBy3 = (num) = num*3;
 const makePositive = (num) = Math.abs;
 const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive) // this is what we want 
 
-multiplyBy3AndAbsolute(-50)
+multiplyBy3AndAbsolute(-50) // why???? Uncaught ReferenceError: num is not defined at <anonymous>:2:21
