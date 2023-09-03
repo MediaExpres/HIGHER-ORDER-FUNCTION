fe100cdc2f938bbc0f811d7e775af7a914a43fd0
Mihai Pavelescu <media.expres@gmail.com>
2023-09-03 09:47:04 +0300

    final

--- COMPOSE.js
+++ COMPOSE.js
@@ -9,3 +9,16 @@ const makePositive = (num) = Math.abs;
 const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive) // this is what we want 
 
 multiplyBy3AndAbsolute(-50) // why???? Uncaught ReferenceError: num is not defined at <anonymous>:2:21
+
+// // pipe = the same as compose but goes from left to right
+// const pipe = (f, g) => (data) => g(f(data))
+// const multiplyBy3 = (num) = num*3;
+// const makePositive = (num) = Math.abs;
+// const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive) // this is what we want 
+
+// multiplyBy3AndAbsolute(-50) // why???? Uncaught ReferenceError: num is not defined at <anonymous>:2:21
+
+fn1(fn2(fn3(50)));
+compose(fn1, fn2, fn3)(50)
+pipe(fn3, fn2, fn1)(50)
+// all the 2 will have the exact same result 
