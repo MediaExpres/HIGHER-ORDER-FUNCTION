ee04e61c3c52e5d53234934999145a9800e0e019
Mihai Pavelescu <media.expres@gmail.com>
2023-09-03 09:28:07 +0300

    writing the compose function

--- COMPOSE.js
+++ COMPOSE.js
@@ -1,6 +1,11 @@
 // Compose - the most important feature of Javascript as per A. Neagoie
 
 // data --> fn --> data --> fn and so on
-// Request: have a number, multyply by 3, absolute
+// Request: have a number, multyply by 3, absolute which is the same to positive
 
-const multiplyBy3AndAbsolute = compose()
+const compose = (f, g) => (Data) => f(g(Data))
+const multiplyBy3 = (num) = num*3;
+const makePositive = (num) = Math.abs;
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive) // this is what we want 
+
+multiplyBy3AndAbsolute(-50)
