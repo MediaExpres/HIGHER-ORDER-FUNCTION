f987d12e80720e48c01ef27e9f514b37840a0da5
Mihai Pavelescu <media.expres@gmail.com>
2023-09-03 08:47:23 +0300

    request added

--- COMPOSE.js
+++ COMPOSE.js
@@ -1,3 +1,6 @@
 // Compose - the most important feature of Javascript as per A. Neagoie
 
 // data --> fn --> data --> fn and so on
+// Request: have a number, multyply by 3, absolute
+
+const multiplyBy3AndAbsolute = compose()
