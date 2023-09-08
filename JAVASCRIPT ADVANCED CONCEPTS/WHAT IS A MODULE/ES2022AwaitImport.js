68d4a27afb543251d4e402228f45525bcbd1862a
Mihai Pavelescu <media.expres@gmail.com>
2023-09-08 15:41:35 +0300

    example

--- JAVASCRIPT ADVANCED CONCEPTS/WHAT IS A MODULE/ES2022AwaitImport.js
+++ JAVASCRIPT ADVANCED CONCEPTS/WHAT IS A MODULE/ES2022AwaitImport.js
@@ -1,7 +1,12 @@
 // const script2 = require('./script2')
-import {largeNumber} from './script2.js'
+// import {largeNumber} from './script2.js'
 
-const a = largeNumber
+import { largeNumber } from './script2.js'
+
+if (1 === 1) {
+    const { largeNumber } = await import ('./script2.js')
+}
+
 const b = 7
 
-console.log(a, b)
+console.log(largeNumber, b)
