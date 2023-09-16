54800ffa3fd0b331b4571b6aa2f5469793766b09
Mihai Pavelescu <media.expres@gmail.com>
2023-09-16 13:17:39 +0200

    added c0mpose and pipe

--- JAVASCRIPT ADVANCED CONCEPTS/FUNCTIONAL PROGRAMMING/compose_pipe_arity.js
+++ JAVASCRIPT ADVANCED CONCEPTS/FUNCTIONAL PROGRAMMING/compose_pipe_arity.js
@@ -4,3 +4,13 @@ fn1(fn2(fn3(50)));
 compose(fn1, fn2, fn3) (50)
 pipe(fn1, fn2, fn3) (50)
 
+// compose
+// pipe
+
+const compose = (f, g) => (data) => f(g(data))
+const pipe = (f, g) => (data) => g(f(data))
+const multiplyBy3 = (num) => mum*3;
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive);
+
+multiplyBy3AndAbsolute(50)
