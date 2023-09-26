b73a4ed82c9a3af1de6107d3c28ea68301b04c95
Mihai Pavelescu <media.expres@gmail.com>
2023-09-26 17:57:31 +0300

    example and speeds

--- JAVASCRIPT ADVANCED CONCEPTS/Data Structures In JavaScript/AraysIntroduction.js
+++ JAVASCRIPT ADVANCED CONCEPTS/Data Structures In JavaScript/AraysIntroduction.js
@@ -1,5 +1,19 @@
 // Data Structures 
 // - arrays
+// are called lists, are stored in contiguous memory
+
+// 0 Juice
+// 1 Apple
+// 2 Cheese
+// 3 Mango 
+// 4 Grapes
+// 5 Eggs
+
+// lookup or access is supper fast O(1)
+// push is O(1)
+// insert O(n)
+// delete 0(n)
+
 // - stacks
 // - queues
 // - linked lists
