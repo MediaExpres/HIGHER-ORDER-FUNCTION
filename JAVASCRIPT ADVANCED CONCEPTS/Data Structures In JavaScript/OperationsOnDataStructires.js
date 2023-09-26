9d21190c42c570d93856e0c08250632fd65cfc90
Mihai Pavelescu <media.expres@gmail.com>
2023-09-26 17:50:12 +0300

    data structures and algorithms added

--- JAVASCRIPT ADVANCED CONCEPTS/Data Structures In JavaScript/OperationsOnDataStructires.js
+++ JAVASCRIPT ADVANCED CONCEPTS/Data Structures In JavaScript/OperationsOnDataStructires.js
@@ -3,3 +3,22 @@
 // Traversal - access each data once so it can be processed
 // Searching
 // Sorting
+
+// ACCESS = the most important 
+// bigocheatsheet.com
+
+// Data Structures 
+// - arrays
+// - stacks
+// - queues
+// - linked lists
+// - Trees 
+// - tries 
+// - graphs
+// - hash tables
+
+// ALGORITHMS 
+// sorting 
+// dinamic promgramming 
+// BFS+DFS (searching) 
+// Recursion
